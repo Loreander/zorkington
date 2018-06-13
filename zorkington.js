4becc536ea8e9e50ba07919d92cc63ed7170cc6c
Loreander <33237304+Loreander@users.noreply.github.com>
2018-06-13 16:50:55 -0400

    big changes

--- zorkington.js
+++ zorkington.js
@@ -1,28 +1,74 @@
 console.log('Your journey begins....\n')
 currentRoom = '182 Main St.';
-roomDescriptions = {'182 Main St.': 'You are standing on Main Street between Church\nand South Winooski.There is a door here. A keypad\nsits on the handle.On the door is a handwritten sign.\n>_'}
+playerInventory = {}
+roomDescriptions = {
+    '182 Main St.': 'You are standing on Main Street between Church\nand South Winooski.There is a door here. A keypad\nsits on the handle.On the door is a handwritten sign.', 
+ 
+    'Foyer': 'You are in a foyer. Or maybe it\'s an antechamber. Or \na vestibule. Or an entryway. Or an atrium. Or a \nnarthex. But let\'s forget all that fancy flatlander \nvocabulary,and just call it a foyer. In Vermont, this \nis pronounced "FO-ee-yurr". A copy of Seven Days lies \nin a corner.'}
+
 arrivesInRoom();
 
 function arrivesInRoom(){
     console.log(roomDescriptions[currentRoom])
     userAction = getUserAction()
-}
-
+};
 
 /* Trying to get user response */
 function getUserAction(){
+    console.log('\n>_')
     process.stdin.once('data', (chunk) => {
     userAction = chunk.toString().trim();
-    checkAction()
-
+    if (currentRoom === '182 Main St.') {
+        checkActionMainSt()
+    } else if (currentRoom === 'Foyer'){
+        checkActionFoyer()
+    }
 });
-}
+};
 
 /* Checking to see if the action matches our list of actions */
-function checkAction(){
+function checkActionMainSt(){
+    console.log('')
     if(userAction === 'read sign'){
         console.log('The sign says "Welcome to Burlington Code Academy! \nCome on up to the second floor. If the door is locked,\nuse the code 12345."')
-    } else console.log('I don\'t understand what you want me to do...')
-}
+        getUserAction()
+    } else if(userAction === 'take sign'){
+        console.log('That would be selfish. How will other students find \ntheir way?')
+        getUserAction()
+    } else if(userAction === 'open door'){
+        console.log('The door is locked. There is a keypad on the door handle')
+        getUserAction()
+    } else if(userAction === 'look around'){
+        arrivesInRoom()
+    } else if(userAction === 'check inventory') {
+        console.log(playerInventory)
+        getUserAction()
+    } else if(userAction === 'enter code 12345' || userAction === 'enter 12345' || userAction === '12345'){
+        console.log('Success! The door opens. You enter the foyer and the \ndoor shuts behind you')
+        currentRoom = 'Foyer'
+        arrivesInRoom()
+    }else error(userAction)
+};
 
+function checkActionFoyer(){
+    if(userAction === 'look around'){
+        arrivesInRoom()
+    } else if(userAction === 'check inventory') {
+        console.log(playerInventory)
+        getUserAction()
+    } else if(userAction === 'take paper' || userAction ==='take seven days'){
+        console.log('You pick up the paper and leaf through it looking for \ncomics and ignoring the articles, just like everybody \nelse does.')
+        addInventory('newspaper', 'Seven Days Newspaper')
+        getUserAction()
+    } else error(userAction)
+};
 
+function error(userAction) {
+    console.log('I don\'t understand how to "' + userAction + '"')
+    getUserAction()
+};
+
+function addInventory(item, description) {
+    playerInventory[item] = description
+    console.log('\nYou have added the ' + item + ' to your inventory!')
+};
