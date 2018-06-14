5bca257508f3f40c39e43ae590a08423c8087cad
Loreander <33237304+Loreander@users.noreply.github.com>
2018-06-14 14:59:14 -0400

    our working beta

--- zorkington.js
+++ zorkington.js
@@ -38,6 +38,8 @@ function checkActionMainSt(){
     } else if(userAction === 'open door'){
         console.log('The door is locked. There is a keypad on the door handle')
         getUserAction()
+    } else if(userAction.slice(0,4) == 'drop'){
+        removeInventory(userAction.slice(5))
     } else if(userAction === 'look around'){
         arrivesInRoom()
     } else if(userAction === 'check inventory' || userAction ==='i' || userAction ==='inventory') {
@@ -46,6 +48,10 @@ function checkActionMainSt(){
         console.log('Success! The door opens. You enter the foyer and the \ndoor shuts behind you')
         currentRoom = 'Foyer'
         arrivesInRoom()
+    } else if(userAction === 'hint'){
+        hints()
+    } else if(userAction === 'help'){
+        displayHelp()
     } else if (userAction.slice(-5) !== '12345' && (userAction.slice(0,10) === 'enter code' || userAction.slice(0,5) === 'enter')) {
         console.log('Bzzzzt! The door is still locked')
         getUserAction()
@@ -57,6 +63,12 @@ function checkActionFoyer(){
         arrivesInRoom()
     } else if(userAction === 'check inventory' || userAction ==='i' || userAction ==='inventory') {
         checkInventory()
+    } else if(userAction === 'help'){
+        displayHelp()
+    } else if(userAction === 'hint'){
+        hints()
+    } else if(userAction.slice(0,4) == 'drop'){
+        removeInventory(userAction.slice(5))
     } else if(userAction === 'take paper' || userAction ==='take seven days'){
         console.log('You pick up the paper and leaf through it looking for \ncomics and ignoring the articles, just like everybody \nelse does.')
         addInventory('newspaper', 'Seven Days Newspaper')
@@ -66,7 +78,7 @@ function checkActionFoyer(){
 };
 
 function error(userAction) {
-    console.log('I don\'t understand how to "' + userAction + '"')
+    console.log('I don\'t understand how to "' + userAction + '", type help if you are stuck.')
     getUserAction()
 };
 
@@ -76,16 +88,45 @@ function addInventory(item, description) {
 };
 
 function displayHelp(){
-
+    console.log('Help Menu')
+    console.log('---------')
+    console.log('Use the following shortcuts if you get stuck...')
+    console.log('look around : Gives you a description of the room')
+    console.log('exits       : gives you a list of exits from the current room')
+    console.log('inventory   : displays player inventory')
+    console.log('hint        : if there is a user hint available')
+    console.log('help        : brings up the help menu')
+    console.log('drop item   : will remove an item from your inventory')
+    getUserAction()
 }
 
 function checkInventory(){
 
-    if (playerInventory === {}){
-        console.log('You are not carrying anything')
-    } 
-
-    else console.log(`You are carrying:`)
-        console.log(playerInventory)
-        getUserAction()
+    if (playerInventory.toString() == ''){
+        console.log('You are not carrying anything...')
+    } else console.log(`You are carrying:`)
+        console.log(playerInventory);
+    getUserAction()
 }
+
+function hints(){
+    if(currentRoom == '182 Main St.'){
+        console.log('I wonder what the sign says?...')
+        getUserAction()
+    }
+    else if(currentRoom == 'Foyer'){
+        console.log('Never miss a chance to grab a Seven Days...')
+        getUserAction()
+    }
+}
+
+function removeInventory(item){
+    if(playerInventory[item]!=undefined){
+    console.log(`You have removed the ${item} from your inventory and it is now at ${currentRoom}`)
+    roomDescriptions[currentRoom] += `The room contains a ${playerInventory[item]}`
+    delete playerInventory[item]
+    }
+    else console.log('This is not an item in your inventory. Use "check inventory" to see your items.');
+    getUserAction()
+}
+
---
+++ zorkington2.js
@@ -0,0 +1,33 @@
+let rooms = {
+    'foyer'       : {
+        'exits'         : ['182 Main St.'],
+        'description'   : 'You are in a foyer. Or maybe it\'s an antechamber. Or \na vestibule. Or an entryway. Or an atrium. Or a \nnarthex. But let\'s forget all that fancy flatlander \nvocabulary,and just call it a foyer. In Vermont, this \nis pronounced "FO-ee-yurr". A copy of Seven Days lies \nin a corner.',
+        'items'         : {'newspaper' : 'A Seven Days Newspaper'},
+        'actions'       : {
+            'take paper' : ''
+        }
+    },
+
+
+    'mainStreet'  : {
+        'exits'         : [],
+        'description'   : 'You are standing on Main Street between Church\nand South Winooski.There is a door here. A keypad\nsits on the handle.On the door is a handwritten sign.',
+        'items'         : [],
+        'actions'       : {
+            'read sign'  : 'The sign says "Welcome to Burlington Code Academy! \nCome on up to the second floor. If the door is locked,\nuse the code 12345."',
+            'take sign'  : 'That would be selfish. How will other students find \ntheir way?',
+            'open door'  : 'The door is locked. There is a keypad on the door handle',
+            'look around':  rooms[currentRoom].description,
+        }
+    },
+}
+
+currentRoom = 'mainStreet'
+console.log('Your journey begins....\n')
+arrivesInRoom()
+
+function arrivesInRoom(){
+    console.log(rooms.currentRoom.description)
+ //   userAction = getUserAction()
+};
+
